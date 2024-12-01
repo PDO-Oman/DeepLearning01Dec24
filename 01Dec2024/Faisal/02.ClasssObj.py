3e6097a333d4281e848342c8397155713a8ed942
FaisalHinai <faisal.hinai@outlook.com>
2024-12-01 11:09:29 +0400

    Check

--- 01Dec2024/Faisal/02.ClasssObj.py
+++ 01Dec2024/Faisal/02.ClasssObj.py
@@ -1,1 +1,20 @@
 # welcome
+# Class and Object
+
+class Car:
+    # Constructor to initialize the object
+    def __init__(self, make, model, year):
+        self.make = make       # Attribute
+        self.model = model     # Attribute
+        self.year = year       # Attribute
+        
+    # Method to display car information
+    def display_info(self):
+        print(f"{self.year} {self.make} {self.model}")
+
+# Creating an object (instance) of the Car class
+my_car = Car("Toyota", "Corolla", 2020)
+
+# Accessing the object's method
+my_car.display_info()  # Output: 2020 Toyota Corolla
+
