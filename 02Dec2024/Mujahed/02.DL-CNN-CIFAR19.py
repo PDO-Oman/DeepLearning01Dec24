d85112e45b8143bc8c8258c65e460e113f94578c
mujahed85 <mujahed.mca@gmail.com>
2024-12-02 14:09:31 +0400

    CIFAR 10 DL CNN

---
+++ 02Dec2024/Mujahed/02.DL-CNN-CIFAR19.py
@@ -0,0 +1,20 @@
+from tensorflow.keras.datasets import cifar10
+
+# Load the CIFAR-10 dataset
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+# Preprocess the data
+x_train = x_train / 255.0  # Normalize to [0, 1]
+x_test = x_test / 255.0
+
+# Build the CNN model
+model = models.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(128, (3, 3), activation='relu'),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(10, activation='softmax')  # 10 classes
+])
