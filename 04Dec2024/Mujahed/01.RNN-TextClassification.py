4d7326314ab8da7274ae47028e1982d3011887c7
mujahed85 <mujahed.mca@gmail.com>
2024-12-04 10:38:44 +0400

    Visualization

--- 04Dec2024/Mujahed/01.RNN-TextClassification.py
+++ 04Dec2024/Mujahed/01.RNN-TextClassification.py
@@ -32,10 +32,23 @@ model.add(Dense(1, activation='sigmoid'))
 # Compile the model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+# Train the model
+history = model.fit(X_train, y_train, epochs=5, batch_size=64, validation_data=(X_test, y_test))
 
 
+# Evaluate the model on test data
+test_loss, test_accuracy = model.evaluate(X_test, y_test)
+print(f'Test Accuracy: {test_accuracy * 100:.2f}%')
 
+# Optionally, plot the training history (accuracy and loss)
+import matplotlib.pyplot as plt
 
-
-
+# Plot training & validation accuracy values
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('Model Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
 
