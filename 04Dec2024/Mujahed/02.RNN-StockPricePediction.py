d96f4b5fffeb7974baef7b44870796b8e29c8001
mujahed85 <mujahed.mca@gmail.com>
2024-12-04 13:41:05 +0400

    Added Requirements.txt for Stock Prediction

--- 04Dec2024/Mujahed/02.RNN-StockPricePediction.py
+++ 04Dec2024/Mujahed/02.RNN-StockPricePediction.py
@@ -1,3 +1,4 @@
+# 01. Load Packages
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -5,3 +6,6 @@ from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
 import yfinance as yf
+
+# 02: Download Stock Data (e.g., Apple)
+stock_data = yf.download('AAPL', start='2010-01-01', end='2024-01-01')
