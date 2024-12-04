be283a8703a0c990aa9b7725036ca63010a0618f
mujahed85 <mujahed.mca@gmail.com>
2024-12-04 14:20:01 +0400

    change dataset name to stock

--- 04Dec2024/Mujahed/KaggleAPI/auth.py
+++ 04Dec2024/Mujahed/KaggleAPI/auth.py
@@ -5,7 +5,7 @@ api = KaggleApi()
 api.authenticate()
 
 # Specify the dataset name (e.g., Titanic dataset)
-dataset_name = 'titanic'
+dataset_name = 'jacksoncrow/stock-market-dataset'
 
 # Download and unzip dataset to the current directory
 api.dataset_download_files(dataset_name, path='./', unzip=True)
